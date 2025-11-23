477c2cd151e8f6813efbac46fc278ca31bca9bdf
sanchez@GitauJW <simon.maina1@student.moringaschool.com>
2025-11-23 14:46:11 +0300

    initial

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,6 +1,38 @@
 // Write your code here
+const products =[]
+const product1= {
+  name: 'laptop'
 
+}
+const product2= {
+  name: 'phone'
 
+}
+const product3= {
+  name: 'headphone'
+
+}
+const product4= {
+  name:'monitor'
+}
+
+function logFirstProduct() {
+  console.log(products[0]);
+}
+
+function addProduct(newProduct) {
+  products.push(newProduct);
+}
+
+function updateProductName(index, newName) {
+  if (products[index]) {
+    products[index].name = newName;
+  }
+}
+
+function removeLastProduct() {
+  products.pop();
+}
 
 // Export the necessary parts for testing
 module.exports = {
